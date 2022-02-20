5c97af02e9dfa8d2895c7c200a4ce6e750be7114
Maximiliano Villa <villa.maximiliano@gmail.com>
2022-02-20 10:38:18 -0300

    EDIT: views converted to classes

--- pms/urls.py
+++ pms/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views_
 
 urlpatterns=[
-    path("",views_.home,name="home"),
-    path("search/room/",views_.room_search,name="search"),
-    path("search/book/",views_.book_search,name="book_search"),
-    path("book/<str:pk>/",views_.book,name="book"),
+    path("",views_.HomeView.as_view(),name="home"),
+    path("search/room/",views_.RoomSearchView.as_view(),name="search"),
+    path("search/book/",views_.BookSearchView.as_view(),name="book_search"),
+    path("book/<str:pk>/",views_.BookView.as_view(),name="book"),
     path("reservations/",views_.reservations,name="reservations")
 ]
--- pms/views_.py
+++ pms/views_.py
@@ -1,80 +1,85 @@
-from datetime import date
+from urllib import request
 from django.shortcuts import render, redirect
-from django.forms import modelformset_factory
+from django.views import View
 from .models import Room
 from .forms import *
-from datetime import datetime
 from django.db.models import F,Q, Count
 from .reservation_code import generate
 from .form_dates import Ymd
-from django.views.decorators.csrf import csrf_protect
+from django.views.decorators.csrf import ensure_csrf_cookie
 # Create your views here.
 
-def book_search(request):
-    query=request.GET.dict()
-    if(not "filter" in query):
-        return redirect("/")
-    books=(Book.objects
-        .filter(Q(code__icontains=query['filter']) | Q(customer__name__icontains=query['filter']))
-        .order_by("-created"))
-    room_search_form=RoomSearchForm()
-    context={
-        'books':books,
-        'form':room_search_form
-    }
-    return render(request,"home.html",context)
+class BookSearchView(View):
 
-def room_search(request):
-    query=request.GET.dict()
-    checkin=Ymd.Ymd(query['checkin'])
-    checkout=Ymd.Ymd(query['checkout'])
-    total_days=checkout-checkin
-
-    filters={
-        'room_type__max_guests__gte':query['guests']
+    def get(self,request):
+        query=request.GET.dict()
+        if(not "filter" in query):
+            return redirect("/")
+        books=(Book.objects
+            .filter(Q(code__icontains=query['filter']) | Q(customer__name__icontains=query['filter']))
+            .order_by("-created"))
+        room_search_form=RoomSearchForm()
+        context={
+            'books':books,
+            'form':room_search_form
         }
-    exclude={
-        'book__checkin__lte':query['checkout'],
-        'book__checkout__gte':query['checkin'],
+        return render(request,"home.html",context)
+
+
+class RoomSearchView(View):
+    def get(self,request):
+        query=request.GET.dict()
+        checkin=Ymd.Ymd(query['checkin'])
+        checkout=Ymd.Ymd(query['checkout'])
+        total_days=checkout-checkin
+
+        filters={
+            'room_type__max_guests__gte':query['guests']
+            }
+        exclude={
+            'book__checkin__lte':query['checkout'],
+            'book__checkout__gte':query['checkin'],
+            }
+        rooms=(Room.objects
+            .filter(**filters)
+            .exclude(**exclude)
+            .annotate(total=total_days*F('room_type__price'))
+            .order_by("room_type__max_guests","name")
+            )
+        total_rooms=(Room.objects
+            .filter(**filters)
+            .values("room_type__name","room_type")
+            .exclude(**exclude)
+            .annotate(total=Count('room_type'))
+            .order_by("room_type__max_guests"))
+        data={
+            'total_days':total_days
         }
-    rooms=(Room.objects
-        .filter(**filters)
-        .exclude(**exclude)
-        .annotate(total=total_days*F('room_type__price'))
-        .order_by("room_type__max_guests","name")
-        )
-    total_rooms=(Room.objects
-        .filter(**filters)
-        .values("room_type__name","room_type")
-        .exclude(**exclude)
-        .annotate(total=Count('room_type'))
-        .order_by("room_type__max_guests"))
-    data={
-        'total_days':total_days
-    }
-    url_query=request.GET.urlencode()
-    context={
-        "rooms":rooms,
-        "total_rooms":total_rooms,
-        "query":query,
-        "url_query":url_query,
-        "data":data
+        url_query=request.GET.urlencode()
+        context={
+            "rooms":rooms,
+            "total_rooms":total_rooms,
+            "query":query,
+            "url_query":url_query,
+            "data":data
+            }
+        return render(request,"search.html",context)
+
+class HomeView(View):
+    def get(self,request):
+        books=Book.objects.all().order_by("-created")
+        room_search_form=RoomSearchForm()
+        context={
+            'books':books,
+            'form':room_search_form
         }
-    return render(request,"search.html",context)
+        return render(request,"home.html",context)
 
+class BookView(View):
+    from django.utils.decorators import method_decorator
+    @method_decorator(ensure_csrf_cookie)
 
-def home(request):
-    books=Book.objects.all().order_by("-created")
-    room_search_form=RoomSearchForm()
-    context={
-        'books':books,
-        'form':room_search_form
-    }
-    return render(request,"home.html",context)
-
-@csrf_protect
-def book(request,pk):
-    if(request.method=="POST"):
+    def post(self, request,pk):
         customer_form=CustomerForm(request.POST,prefix="customer")
         if customer_form.is_valid():
             customer=customer_form.save()
@@ -88,21 +93,25 @@ def book(request,pk):
                 book_form.save()
                 
         return redirect('/')
-    query=request.GET.dict()
-    room=Room.objects.get(id=pk)
-    checkin=Ymd.Ymd(query['checkin'])
-    checkout=Ymd.Ymd(query['checkout'])
-    total_days=checkout-checkin
-    total=total_days*room.room_type.price
-    print(total)
-    query['total']=total
-    context={
-        "url_query":request.GET.urlencode(),
-        "room":room,
-        "book_form":BookFormExcluded(prefix="book",initial=query),
-        "customer_form":CustomerForm(prefix="customer")
-        }
-    return render(request,"book.html",context)
+
+    def get(self,request,pk):
+        query=request.GET.dict()
+        room=Room.objects.get(id=pk)
+        checkin=Ymd.Ymd(query['checkin'])
+        checkout=Ymd.Ymd(query['checkout'])
+        total_days=checkout-checkin
+        total=total_days*room.room_type.price
+        print(total)
+        query['total']=total
+        context={
+            "url_query":request.GET.urlencode(),
+            "room":room,
+            "book_form":BookFormExcluded(prefix="book",initial=query),
+            "customer_form":CustomerForm(prefix="customer")
+            }
+        return render(request,"book.html",context)
+
+    
 
 
 def reservations(request):
