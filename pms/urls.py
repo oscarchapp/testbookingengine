5e4dbc98e834d0114c47bda015d5aefb3cadca0d
Maximiliano Villa <villa.maximiliano@gmail.com>
2022-02-22 12:18:16 -0300

    CHANGE: filenames

--- pms/urls.py
+++ pms/urls.py
@@ -1,14 +1,14 @@
 from django.urls import path
-from . import views_
+from . import views
 
 urlpatterns = [
-    path("",views_.HomeView.as_view(),name = "home"),
-    path("search/room/",views_.RoomSearchView.as_view(),name = "search"),
-    path("search/booking/",views_.BookingSearchView.as_view(),name = "booking_search"),
-    path("booking/<str:pk>/",views_.BookingView.as_view(),name = "booking"),
-    path("booking/<str:pk>/edit",views_.EditBookingView.as_view(),name = "edit_booking"),
-    path("booking/<str:pk>/delete",views_.DeleteBookingView.as_view(),name = "delete_booking"),
-    path("rooms/",views_.RoomsView.as_view(),name = "rooms"),
-    path("room/<str:pk>/",views_.RoomView.as_view(),name = "room_details"),
-    path("dashboard/",views_.DashboardView.as_view(),name = "dashboard")
+    path("",views.HomeView.as_view(),name = "home"),
+    path("search/room/",views.RoomSearchView.as_view(),name = "search"),
+    path("search/booking/",views.BookingSearchView.as_view(),name = "booking_search"),
+    path("booking/<str:pk>/",views.BookingView.as_view(),name = "booking"),
+    path("booking/<str:pk>/edit",views.EditBookingView.as_view(),name = "edit_booking"),
+    path("booking/<str:pk>/delete",views.DeleteBookingView.as_view(),name = "delete_booking"),
+    path("rooms/",views.RoomsView.as_view(),name = "rooms"),
+    path("room/<str:pk>/",views.RoomView.as_view(),name = "room_details"),
+    path("dashboard/",views.DashboardView.as_view(),name = "dashboard")
 ]
