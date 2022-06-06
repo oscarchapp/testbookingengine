9be99591a4a7322f506f12c5f15dca756e75d29b
Oscar Verona <oscar.verona@chappsolutions.com>
2022-06-06 09:41:13 +0100

    Reformat

--- chapp/settings.py
+++ chapp/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -25,8 +24,7 @@ SECRET_KEY = 'django-insecure-v&=r2u21il_nifb5(g%z!lnf!@dq1*s@j&*f%_mlndmh7ivqj9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['chapp-inn.herokuapp.com','127.0.0.1', 'localhost']
-
+ALLOWED_HOSTS = ['chapp-inn.herokuapp.com', '127.0.0.1', 'localhost']
 
 # Application definition
 
@@ -72,7 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'chapp.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
@@ -82,7 +79,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -102,7 +98,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -110,18 +105,17 @@ LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
 
-USE_I18N = True
+USE_I18N = False
 
-USE_TZ = True
-
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS=[BASE_DIR/'pms/statics']
-STATIC_ROOT=BASE_DIR/'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'pms/statics']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
--- pms/admin.py
+++ pms/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
 from .models import Room, Booking, Customer, Room_type
-# Register your models here.
 
-admin.site.register([Room, Booking, Customer,Room_type])
+admin.site.register([Room, Booking, Customer, Room_type])
